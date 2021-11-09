d02b664febb0fcc7afd5fb5b09c4cf59428b6c8b
cerbe <cerberusprogrammer@gmail.com>
2021-11-08 16:09:24 -0800

    Implementacion del modulo Start.java y Start.fxml, estan terminado, estan pendientes de comunicarse con otros modulos.

--- src/start/Start.java
+++ src/start/Start.java
@@ -8,6 +8,9 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Start {
 
@@ -17,8 +20,8 @@ public class Start {
     private Button hashButton;
     @FXML
     private Button binButton;
-    @FXML
-    private Button importButton;
+
+    public static ArrayList<String> stringArrayList = new ArrayList<>();
 
     @FXML
     void importFile(ActionEvent event) throws IOException {
@@ -31,17 +34,28 @@ public class Start {
 
         File file = fileChooser.showOpenDialog(new Stage());
 
-        String string;
-        FileReader fileReader = new FileReader(file);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-        while ((string = bufferedReader.readLine()) != null)
-            System.out.println(string);
-
-        bufferedReader.close();
-
         labelExample.setVisible(true);
         binButton.setDisable(false);
         hashButton.setDisable(false);
+
+        separator(file);
+    }
+
+    void separator(File file) throws IOException {
+        String s1;
+        String s2;
+
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        while ((s1 = bufferedReader.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer (s1);
+
+            while (st.hasMoreTokens()) {
+                s2 = st.nextToken();
+                s2 = s2.replaceAll("[^a-zA-ZÀ-ÿ\\u00f1\\u00d1]", "");
+                stringArrayList.add(s2);
+            }
+        }
     }
 }
